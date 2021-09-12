cc76732414a1d6d6b06495e3b3c2679144675bdb
Omar Elewa <omar2@omar2.local>
2021-09-13 01:11:25 +0200

    Building a React App 1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<Hello />,
   document.getElementById('root')
 );
 
